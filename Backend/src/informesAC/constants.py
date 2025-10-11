f462397baf907a0621fe9381a83e38356ee929ca
Santiago <santiagocorvalan2003@gmail.com>
2025-10-11 19:45:54 -0300

    Backend completo funcionando, agregado de algunos comentarios indicativos. (Falta frontend).

---
+++ Backend/src/informesAC/constants.py
@@ -0,0 +1,3 @@
+class ErrorCode:
+    INFORMES_NO_EXISTEN = "No hay informes curriculares existentes."
+
--- Backend/src/informesAC/services.py
+++ Backend/src/informesAC/services.py
@@ -1,6 +1,7 @@
+from typing import List
 from sqlalchemy.orm import Session
 from src.informesAC.models import InformesAC
-from typing import List
+from src.informesAC import exceptions
 
 class InformeACService:
     def __init__(self, db: Session):
@@ -8,3 +9,16 @@ class InformeACService:
 
     def obtener_historial_completo(self) -> List[InformesAC]:
         return self.db.query(InformesAC).order_by(InformesAC.anio.desc()).all()
+
+    def obtener_informes_por_docente(self, id_docente: int) -> List[InformesAC]:
+        informes = (
+            self.db.query(InformesAC)
+            .filter(InformesAC.id_docente == id_docente)
+            .order_by(InformesAC.anio.desc())
+            .all()
+        )
+
+        if not informes:
+            raise exceptions.InformesNoEncontrados()
+
+        return informes
