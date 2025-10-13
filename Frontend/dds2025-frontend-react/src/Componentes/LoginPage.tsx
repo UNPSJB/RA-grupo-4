f69298c7017c86d72ee3a8f67c4cda176cfe1990
Dennis James <dennisjms999@gmail.com>
2025-10-13 15:51:39 -0300

    cambios en imagen de login

--- Frontend/dds2025-frontend-react/src/Componentes/LoginPage.tsx
+++ Frontend/dds2025-frontend-react/src/Componentes/LoginPage.tsx
@@ -13,10 +13,12 @@ function LoginPage() {
     <div className="login-container">
       <div className="login-box">
         <img
-          src="https://cdn-icons-png.flaticon.com/512/847/847969.png"
+          // src="https://cdn-icons-png.flaticon.com/512/847/847969.png"
+          src="https://www.oeco.org.br/wp-content/uploads/2015/04/22102010-albatroz-de-sobrancelha.jpg"
           alt="Usuario"
           className="login-avatar"
         />
+        <p className="admin-text">Admin</p>
         <button className="login-button" onClick={handleLogin}>
           Ingresar
         </button>
